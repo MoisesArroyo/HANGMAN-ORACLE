a3861c6d52e277ea051de098e22841c91e074d40
Moises Arroyo <marroyor1901@alumno.ipn.mx>
2022-09-26 15:11:31 -0500

    first commit

---
+++ canvas.js
@@ -0,0 +1,53 @@
+function dibujarCanvas(){
+    tablero.lineWidth = 8;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#8A3871";
+
+    tablero.fillRect(0,0,1220,860);
+    tablero.beginPath();
+    tablero.moveTo(650, 500);
+    tablero.lineTo(900, 500);
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function dibujarLinea(){
+    
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.strokeStyle = "#8A3871";
+
+    let anchura=600/palabraSecreto.length;
+    for(let i=0; i<palabraSecreto.length; i++){
+        tablero.moveTo(500 + (anchura*i), 640);
+        tablero.lineTo(550 + (anchura*i), 640);
+    }
+    tablero.stroke();
+    tablero.closePath();
+}
+
+function escribirLetraCorrecta(index){
+    tablero.font = 'bold 63px Inter';
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    
+    let anchura = 600/palabraSecreto.length;
+    tablero.fillText(palabraSecreto[index], 505+(anchura*index),620);
+    tablero.stroke();
+}
+
+function escribirLetraIncorrecta(letra, errorsLeft){
+    tablero.font = 'bold 40px Inter';
+    tablero.lineWidth = 6;
+    tablero.lineCap = "round";
+    tablero.lineJoin = "round";
+    tablero.fillStyle = "#F3F5F6";
+    tablero.fillText(letra, 535+(40*(10-errorsLeft)), 710, 40);
+
+}
---
+++ index.js
@@ -0,0 +1,55 @@
+let palabras = ["ALURA", "ORACLE", "ONE", "JAVASCRIPT", "HTML", "CSS"];
+let tablero = document.getElementById("forca").getContext("2d");
+let palabraSecreto = "";
+let letras = [];
+
+let errores = 8;
+
+//Palabra secreta 
+
+function escogerPalabraSecreta(){
+    let palabra = palabras[Math.floor(Math.random()*palabras.length)];
+    palabraSecreto = palabra;
+    console.log(palabraSecreto);
+}
+//ComprobarLetras
+function comprobarLetra(key){
+    let estado = false;
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
+        letras.push(key);
+        console.log(key);
+        return estado;
+    }else{
+        estado=true;
+        console.log(key);
+        return estado;
+    }
+}
+function anadirLetraErrores(){
+    errores -= 1;
+    console.log(errores);
+}
+
+
+//Iniciar   
+function iniciarJuego(){
+    document.getElementById("iniciar-juego").style.display = "none";
+    escogerPalabraSecreta();
+    dibujarCanvas();
+    dibujarLinea();
+
+    document.onkeydown = (e) => {
+        let letra = e.key.toUpperCase();
+        
+        if(comprobarLetra(letra) && palabraSecreto.includes(letra)){
+            for(let i = 0; i<palabraSecreto.length; i++){
+                if(palabraSecreto[i] === letra){
+                    escribirLetraCorrecta(i);
+                }
+            }
+        }else{
+            anadirLetraErrores(letra);
+            escribirLetraIncorrecta(letra, errores);
+        }
+    }
+}
